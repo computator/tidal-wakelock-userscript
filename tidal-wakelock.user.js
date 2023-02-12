93c91fa0fbd53672ae24b5321ac9a979b46b5c7c
Reuben Lifshay <rlifshay@gmail.com>
2023-02-11 23:26:18 -0800

    Add status indicator

--- tidal-wakelock.user.js
+++ tidal-wakelock.user.js
@@ -3,7 +3,7 @@
 // @name Tidal Playback Wakelock
 // @description Sets a wakelock while Tidal is playing to keep the screen unlocked
 // @match https://listen.tidal.com/*
-// @version 0.1
+// @version 0.2
 // ==/UserScript==
 
 class TidalPlaybackWatcher extends EventTarget {
@@ -83,13 +83,14 @@ class TidalPlaybackWatcher extends EventTarget {
 	}
 }
 
-class TidalWakelock {
+class TidalWakelock extends EventTarget {
 	watcher;
 	wlock = null;
 	wlock_tgt_state = false;
 	update_promise = Promise.resolve();
 
 	constructor(player) {
+		super();
 		this.watcher = new TidalPlaybackWatcher(player);
 		this.watcher.addEventListener('play', this.get_lock);
 		this.watcher.addEventListener('pause', this.release_lock);
@@ -116,12 +117,15 @@ class TidalWakelock {
 
 		if(this.wlock_tgt_state) {
 			console.info(`${this.constructor.name}: Requesting wakelock`);
+			this.dispatchEvent(new CustomEvent('locking'));
 			this.wlock = await navigator.wakeLock.request('screen');
 			console.debug(`${this.constructor.name}: wakelock acquired`);
+			this.dispatchEvent(new CustomEvent('locked'));
 			this.wlock.addEventListener('release', this.lock_released);
 		}
 		else {
 			console.info(`${this.constructor.name}: Releasing wakelock`);
+			this.dispatchEvent(new CustomEvent('releasing'));
 			await this.wlock.release();
 			console.debug(`${this.constructor.name}: Release request complete`);
 		}
@@ -136,8 +140,122 @@ class TidalWakelock {
 		// relock if released by browser
 		if(this.wlock_tgt_state) {
 			console.debug(`${this.constructor.name}: Queueing relock`);
+			this.dispatchEvent(new CustomEvent('relocking'));
 			setTimeout(this.update_wlock, 1000);
 		}
+		else
+			this.dispatchEvent(new CustomEvent('released'));
+	}
+
+	get state() {
+		if(Boolean(this.wlock) != this.wlock_tgt_state)
+			return this.wlock_tgt_state ? 'locking' : 'unlocking';
+		else
+			return this.wlock_tgt_state ? 'locked' : 'unlocked';
+	}
+}
+
+class WakelockStatusComponent {
+	player;
+	wlock;
+	node;
+
+	static css_text = `
+			<style>
+				.wakelock-status-component {
+					display: block;
+					box-sizing: border-box;
+					position: absolute;
+					bottom: 5px;
+					left: 5px;
+					width: 10px;
+					height: 10px;
+					border-radius: 50%;
+					background: var(--indicator-color, #c00);
+					box-shadow: 0 0 4px 1px var(--indicator-color, #c00);
+				}
+
+				.wakelock-status-component[data-state=idle] {
+					--indicator-color: #555;
+				}
+
+				.wakelock-status-component[data-state=working] {
+					--indicator-color: #ca3;
+					animation: 0.6s ease-in infinite alternate wakelock-status-component_fade;
+				}
+
+				.wakelock-status-component[data-state=locked] {
+					--indicator-color: #181;
+				}
+
+				@keyframes wakelock-status-component_fade {
+					from {
+						filter: opacity(100%);
+					}
+
+					to {
+						filter: opacity(0%);
+					}
+				}
+			</style>
+		`;
+	static html_text = `
+			<aside class="wakelock-status-component" title="Screen wakelock status"></aside>
+		`;
+
+	static css_tpl;
+	static html_tpl;
+	static css_injected = false;
+
+	constructor(player, tidal_wlock) {
+		this.player = player;
+		this.wlock = tidal_wlock;
+
+		this.init_tpls();
+		this.attach();
+		this.set_working();
+		this.bind_events();
+		const state = this.wlock.state;
+		if(state == 'locking' || state == 'unlocking')
+			this.set_working();
+		else if(state == 'locked')
+			this.set_locked();
+		else
+			this.set_idle();
+	}
+
+	init_tpls() {
+		if(!this.constructor.css_tpl) {
+			this.constructor.css_tpl = document.createElement('template');
+			this.constructor.css_tpl.innerHTML = this.constructor.css_text;
+		}
+		if(!this.constructor.html_tpl) {
+			this.constructor.html_tpl = document.createElement('template');
+			this.constructor.html_tpl.innerHTML = this.constructor.html_text;
+		}
+	}
+
+	attach() {
+		if(!this.constructor.css_injected)
+			document.head.append(document.importNode(this.constructor.css_tpl.content, true));
+		if(this.node)
+			this.node.remove();
+		let tpl = document.importNode(this.constructor.html_tpl.content, true);
+		this.node = tpl.querySelector('.wakelock-status-component');
+		this.player.append(tpl);
+	}
+
+	set_working = () => { this.node.dataset.state = 'working'; }
+	set_locked = () => { this.node.dataset.state = 'locked'; }
+	set_idle = () => { this.node.dataset.state = 'idle'; }
+
+	bind_events() {
+		this.wlock.addEventListener('locking', this.set_working);
+		this.wlock.addEventListener('relocking', this.set_working);
+
+		this.wlock.addEventListener('locked', this.set_locked);
+
+		this.wlock.addEventListener('released', this.set_idle);
 	}
 }
 
@@ -157,8 +275,10 @@ attach_handle = setInterval(() => {
 			}
 			console.debug("Player found: %o", player);
 			clearTimeout(attach_handle);
-			new TidalWakelock(player);
+			let wl = new TidalWakelock(player);
 			console.info("TidalWakelock attached to player");
+			new WakelockStatusComponent(player, wl);
+			console.debug("WakelockStatusComponent attached to player");
 		}
 		catch (e) {
 			console.error(e);
